3188e8ada01ac52ed07d3da6dc901a05ec042b5e
GregoryBS <aanonimov117@gmail.com>
2020-03-25 17:27:28 +0000

    Fixing diff_test.sh

--- dz2/diff_test.sh
+++ dz2/diff_test.sh
@@ -1,11 +1,12 @@
 ./build/st_main.out tests a > stat.txt
 ./build/dyn_main.out tests a > dyn.txt
-diff stat.txt dyn.txt
-
+cat stat.txt
+cat dyn.txt
 ./build/st_main.out tests You > stat.txt
 ./build/dyn_main.out tests You > dyn.txt
-diff stat.txt dyn.txt
-
+cat stat.txt
+cat dyn.txt
 ./build/st_main.out tests _ancbschsd > stat.txt
 ./build/dyn_main.out tests _ancbschsd > dyn.txt
-diff stat.txt dyn.txt
+cat stat.txt
+cat dyn.txt?
